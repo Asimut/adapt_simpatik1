5c67ec415dd0b86d194565253248589858369318
Asimut <asimut007@gmail.com>
2021-01-11 13:13:01 +0200

    Oleg

---
+++ story_content/user 3.js
@@ -0,0 +1,26 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6LlmF49HLCX":
+        Script1();
+        break;
+  }
+}
+
+function Script1()
+{
+  var player = GetPlayer();
+ 
+var myName = lmsAPI.GetStudentName();
+
+var array = myName.split(' ');
+ 
+// variables for JavaScript
+
+var Name = array[1];
+ 
+// variables in Storyline set to variables from JS above
+player.SetVar("Name", Name);
+}
+
